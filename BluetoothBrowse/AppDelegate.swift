1ea6adc94253baeed98e5ea7d57e6bf6973df76a
Lee Danilek <lee.danilek@yale.edu>
2015-12-24 09:07:33 -0500

    Initial Commit

---
+++ BluetoothBrowse/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BluetoothBrowse
+//
+//  Created by Lee Danilek on 12/24/15.
+//  Copyright © 2015 Ship Shape. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ BluetoothBrowse/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BluetoothBrowse
+//
+//  Created by Lee Danilek on 12/24/15.
+//  Copyright © 2015 Ship Shape. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
